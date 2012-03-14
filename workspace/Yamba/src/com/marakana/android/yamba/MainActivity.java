90b8f10e730bbe3930e340489ee1db3889964fce
Ken Jones <ken.jones@marakana.com>
2012-03-14 12:36:12 +0530

    Added options menu to Yamba

--- workspace/Yamba/src/com/marakana/android/yamba/MainActivity.java
+++ workspace/Yamba/src/com/marakana/android/yamba/MainActivity.java
@@ -3,6 +3,8 @@ package com.marakana.android.yamba;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
 	private static final String TAG = "MainActivity";
@@ -13,5 +15,25 @@ public class MainActivity extends FragmentActivity {
         Log.v(TAG, "onCreate() invoked");
         setContentView(R.layout.main);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.options_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		switch (id) {
+		case R.id.menu_preferences:
+			// Display the PrefsActivity
+			Log.v(TAG, "Settings menu item selected");
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
 
