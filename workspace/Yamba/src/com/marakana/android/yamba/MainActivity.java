3159f70a50ac58dc089597464ffebbc22afdb461
Ken Jones <ken.jones@marakana.com>
2012-03-16 11:13:29 +0530

    Added dynamic management of ComposeFragment and TimelineFragment.

--- workspace/Yamba/src/com/marakana/android/yamba/MainActivity.java
+++ workspace/Yamba/src/com/marakana/android/yamba/MainActivity.java
@@ -3,24 +3,53 @@ package com.marakana.android.yamba;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
 	private static final String TAG = "MainActivity";
+	
+	private ComposeFragment mComposeFragment;
+	private TimelineFragment mTimelineFragment;
+	private FragmentManager mFragmentManager;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.v(TAG, "onCreate() invoked");
         setContentView(R.layout.main);
+        
+        mFragmentManager = getSupportFragmentManager();
+        
+        // Set up the activity's fragments
+        if (savedInstanceState == null) {
+        	// Initial activity creation. Create fragments.
+        	mComposeFragment = new ComposeFragment();
+        	mTimelineFragment = new TimelineFragment();
+        	
+        	// Add the fragments to the layout, and hide the ComposeFragment.
+        	mFragmentManager.beginTransaction()
+        	.add(R.id.fragment_container, mComposeFragment, "compose")
+        	.add(R.id.fragment_container, mTimelineFragment, "timeline")
+        	.hide(mComposeFragment)
+        	.commit();
+        }
+        else {
+        	// Activity bounced. Find the re-created fragments.
+        	mComposeFragment = (ComposeFragment) mFragmentManager.findFragmentByTag("compose");
+        	mTimelineFragment = (TimelineFragment) mFragmentManager.findFragmentByTag("timeline");
+        }
     }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-		getMenuInflater().inflate(R.menu.options_main, menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_main, menu);
+		inflater.inflate(R.menu.options_main_fragment_controls, menu);
 		return true;
 	}
 
@@ -35,14 +64,31 @@ public class MainActivity extends FragmentActivity {
 			startActivity(intent);
 			Log.v(TAG, "Settings menu item selected");
 			return true;
-		case R.id.menu_refresh:
-			// Start UpdaterService to refresh the timeline
-			intent = new Intent(this, UpdaterService.class);
-			startService(intent);
+		case R.id.menu_compose:
+			// Show the ComposeFragment
+			showComposeFragment();
+			return true;
+		case R.id.menu_timeline:
+			// Show the TimelineFragment
+			showTimelineFragment();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+	
+	private void showComposeFragment() {
+		mFragmentManager.beginTransaction()
+			.hide(mTimelineFragment)
+			.show(mComposeFragment)
+			.commit();
+	}
+	
+	private void showTimelineFragment() {
+		mFragmentManager.beginTransaction()
+			.hide(mComposeFragment)
+			.show(mTimelineFragment)
+			.commit();
 	}
 }
 
--- workspace/Yamba/src/com/marakana/android/yamba/TimelineFragment.java
+++ workspace/Yamba/src/com/marakana/android/yamba/TimelineFragment.java
@@ -1,11 +1,19 @@
 package com.marakana.android.yamba;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class TimelineFragment extends ListFragment {
+public class TimelineFragment extends ListFragment
+				implements SimpleCursorAdapter.ViewBinder {
 
 	private static final String[] FROM = {
 		TimelineHelper.KEY_USER,
@@ -23,6 +31,12 @@ public class TimelineFragment extends ListFragment {
 	private SimpleCursorAdapter mAdapter;
 
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
+	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		YambaApplication app = YambaApplication.getInstance();
@@ -35,6 +49,7 @@ public class TimelineFragment extends ListFragment {
 		// Create an adapter mapping the cursor data to the layout
 		mAdapter = new SimpleCursorAdapter(getActivity(),
 				R.layout.timeline_row, mCursor, FROM, TO, 0);
+		mAdapter.setViewBinder(this);
 		
 		setListAdapter(mAdapter);
 	}
@@ -56,6 +71,41 @@ public class TimelineFragment extends ListFragment {
 	public void onDestroy() {
 		super.onDestroy();
 		mCursor.close();
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.options_timeline_fragment, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		switch (id) {
+		case R.id.menu_refresh:
+			// Start UpdaterService to refresh the timeline
+			Intent intent = new Intent(getActivity(), UpdaterService.class);
+			getActivity().startService(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public boolean setViewValue(View v, Cursor cursor, int columnIndex) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.data_date:
+			// Present the date in a more human-friendly fashion
+			TextView tv = (TextView) v;
+			long timestamp = cursor.getLong(columnIndex);
+			CharSequence relTime = DateUtils.getRelativeTimeSpanString(timestamp);
+			tv.setText(relTime);
+			return true;
+		default:
+			return false;
+		}
 	}
 	
 	
